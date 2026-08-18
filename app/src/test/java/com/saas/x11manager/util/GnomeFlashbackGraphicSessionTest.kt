b311734fd625e072c90dc3e2eef19f66629942a7
SaaSD3v <hearesaas@gmail.com>
2026-08-18 00:53:04 -0400

    test: align gnome flashback apt policy

--- app/src/test/java/com/saas/x11manager/util/GnomeFlashbackGraphicSessionTest.kt
+++ app/src/test/java/com/saas/x11manager/util/GnomeFlashbackGraphicSessionTest.kt
@@ -22,7 +22,7 @@ class GnomeFlashbackGraphicSessionTest {
     }
 
     @Test
-    fun aptInstallKeepsAllRecommendedPackagesWithoutSuppression() {
+    fun aptInstallSuppressesRecommendedPackagesByDefault() {
         val plan = requireNotNull(
             GraphicSessionInstallPlans.forSelection(
                 ContainerPlatform.UBUNTU,
@@ -35,8 +35,9 @@ class GnomeFlashbackGraphicSessionTest {
             }
         )
 
-        assertTrue(plan.installRecommendedPackages)
-        assertTrue(install.command.contains("--install-recommends"))
+        assertFalse(plan.installRecommendedPackages)
+        assertTrue(install.command.contains("--no-install-recommends"))
+        assertFalse(install.command.contains("--install-recommends"))
         assertTrue(install.command.contains("gnome-session-flashback dbus-x11 xterm"))
         assertFalse(install.command.contains("blocked_recommends"))
         assertFalse(install.command.contains("\$pkg-"))
