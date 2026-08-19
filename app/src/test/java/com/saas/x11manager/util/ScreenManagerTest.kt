56cb826b121e0ff4c1bdcb10965551db33d0291b
SaaSD3v <hearesaas@gmail.com>
2026-08-18 23:36:34 -0400

    test: cover Screen preference contract

---
+++ app/src/test/java/com/saas/x11manager/util/ScreenManagerTest.kt
@@ -0,0 +1,55 @@
+package com.saas.x11manager.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ScreenManagerTest {
+
+    @Test
+    fun preferencePayloadMatchesEmbeddedLorieContract() {
+        val config = ScreenConfig(
+            resolutionMode = ScreenResolutionMode.Scaled,
+            scalePercent = 140,
+            exactResolution = "1920x1080",
+            customResolution = "1600x900",
+            filtering = ScreenFiltering.Bilinear,
+            stretch = true,
+            fullscreen = true,
+            orientation = ScreenOrientation.Landscape,
+            hideCutout = true,
+            clipboard = false,
+            touchMode = ScreenTouchMode.DirectTouch,
+            keepScreenAwake = true,
+            showAdditionalKeyboard = false
+        )
+
+        val payload = ScreenManager.buildPreferencePayload(config)
+
+        assertEquals("scaled", payload["displayResolutionMode"])
+        assertEquals("140", payload["displayScale"])
+        assertEquals("1920x1080", payload["displayResolutionExact"])
+        assertEquals("1600x900", payload["displayResolutionCustom"])
+        assertEquals("bilinear", payload["displayFilteringMode"])
+        assertEquals("true", payload["displayStretch"])
+        assertEquals("true", payload["fullscreen"])
+        assertEquals("landscape", payload["forceOrientation"])
+        assertEquals("true", payload["hideCutout"])
+        assertEquals("false", payload["clipboardEnable"])
+        assertEquals("3", payload["touchMode"])
+        assertEquals("never", payload["screenIdleTimeout"])
+        assertEquals("false", payload["showAdditionalKbd"])
+    }
+
+    @Test
+    fun invalidValuesAreNormalizedBeforeTheyReachLorie() {
+        val normalized = ScreenConfig(
+            scalePercent = 999,
+            exactResolution = "not-supported",
+            customResolution = "broken"
+        ).normalized()
+
+        assertEquals(300, normalized.scalePercent)
+        assertEquals("1280x1024", normalized.exactResolution)
+        assertEquals("1280x1024", normalized.customResolution)
+    }
+}
