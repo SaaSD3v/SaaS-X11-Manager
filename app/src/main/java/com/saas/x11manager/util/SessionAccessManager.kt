3db4b7282cf4c71844476e65b04ebb85805c6f71
SaaSD3v <hearesaas@gmail.com>
2026-09-05 01:29:53 -0400

    Pass configured session to user launcher preparation

--- app/src/main/java/com/saas/x11manager/util/SessionAccessManager.kt
+++ app/src/main/java/com/saas/x11manager/util/SessionAccessManager.kt
@@ -24,6 +24,7 @@ object SessionAccessManager {
 
         val userPreparation = GraphicSessionUserManager.prepareForStart(
             containerName = containerName,
+            session = session,
             logger = logger
         ) ?: return false
 
