9b09aee82b87a55cfca10977fd81622cc4712d4e
SaaSD3v <hearesaas@gmail.com>
2026-09-19 17:20:28 -0300

    Guard Termux policy cache refresh

--- app/src/test/java/com/saas/x11manager/util/TermuxAppContextPolicyTest.kt
+++ app/src/test/java/com/saas/x11manager/util/TermuxAppContextPolicyTest.kt
@@ -29,6 +29,8 @@ class TermuxAppContextPolicyTest {
         assertTrue(broker.contains("com.termux.app.RunCommandService"))
         assertTrue(broker.contains("am startservice --user 0"))
         assertTrue(broker.contains("allow-external-apps=true"))
+        assertTrue(broker.contains("POLICY_CACHE_MARKER"))
+        assertTrue(broker.contains("TermuxAppSharedProperties"))
         assertTrue(manifest.contains("com.termux.permission.RUN_COMMAND"))
         assertTrue(manifest.contains("<package android:name=\"com.termux\""))
 
