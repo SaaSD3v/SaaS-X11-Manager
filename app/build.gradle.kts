2e545d82bbcd0ec5c197b8d8cdc0c5c7191fb478
SaaSD3v <hearesaas@gmail.com>
2026-09-24 19:06:33 -0300

    Pin Compose runtime tracing for existing Compose stack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,7 +124,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.runtime:runtime-tracing")
+    implementation("androidx.compose.runtime:runtime-tracing:1.0.0-beta01")
 
     implementation("com.google.android.material:material:1.11.0")
 
