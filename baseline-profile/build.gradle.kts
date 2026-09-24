1755bf2889e527273536da7a056882fd12835a7f
SaaSD3v <hearesaas@gmail.com>
2026-09-24 18:57:21 -0300

    Add baseline profile module

---
+++ baseline-profile/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.test")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.baselineprofile")
+}
+
+android {
+    namespace = "com.saas.x11manager.baselineprofile"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 34
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    targetProjectPath = ":app"
+
+    testOptions.managedDevices.devices {
+        create<com.android.build.api.dsl.ManagedVirtualDevice>("pixel6Api34") {
+            device = "Pixel 6"
+            apiLevel = 34
+            systemImageSource = "aosp"
+        }
+    }
+}
+
+baselineProfile {
+    managedDevices += "pixel6Api34"
+    useConnectedDevices = false
+}
+
+dependencies {
+    implementation("androidx.benchmark:benchmark-macro-junit4:1.5.0")
+    implementation("androidx.test:runner:1.7.0")
+    implementation("androidx.test.ext:junit:1.3.0")
+    implementation("androidx.test.uiautomator:uiautomator:2.4.0")
+}
