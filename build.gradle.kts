1e70db95a78059ea3f785f977beefc92d1830e6f
SaaSD3v <hearesaas@gmail.com>
2026-09-24 18:55:59 -0300

    Add runtime performance profiling toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.library") version "8.13.2" apply false
+    id("com.android.test") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("androidx.baselineprofile") version "1.5.0" apply false
 }
