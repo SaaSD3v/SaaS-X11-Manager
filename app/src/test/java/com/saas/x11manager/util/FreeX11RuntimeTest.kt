76783c48bd557d55937ebf7c8faf61c4bc2f4242
SaaSD3v <hearesaas@gmail.com>
2026-09-24 04:51:44 -0300

    Test Free X11 DISPLAY export

---
+++ app/src/test/java/com/saas/x11manager/util/FreeX11RuntimeTest.kt
@@ -0,0 +1,14 @@
+package com.saas.x11manager.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FreeX11RuntimeTest {
+
+    @Test
+    fun exportCommandUsesTheAssignedDisplayVerbatim() {
+        assertEquals("export DISPLAY=:0", FreeX11Runtime.exportCommand(":0"))
+        assertEquals("export DISPLAY=:3", FreeX11Runtime.exportCommand(":3"))
+        assertEquals("export DISPLAY=:12", FreeX11Runtime.exportCommand(":12"))
+    }
+}
