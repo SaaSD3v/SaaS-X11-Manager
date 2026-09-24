7c4bda8219a328dceba16f266b7e6f552c53a43d
SaaSD3v <hearesaas@gmail.com>
2026-09-24 05:32:25 -0300

    Test Free DISPLAY set and unset commands

--- app/src/test/java/com/saas/x11manager/util/FreeX11RuntimeTest.kt
+++ app/src/test/java/com/saas/x11manager/util/FreeX11RuntimeTest.kt
@@ -8,5 +8,7 @@ class FreeX11RuntimeTest {
     @Test
     fun exportCommandIsAlwaysFixedX0ForTheOnlyVariant() {
         assertEquals("export DISPLAY=:0", FreeX11Runtime.exportCommand(":0"))
+        assertEquals("unset DISPLAY", FreeX11Runtime.unsetCommand())
+        assertEquals("unset DISPLAY; export DISPLAY=:0", FreeX11Runtime.replaceCommand(":0"))
     }
 }
